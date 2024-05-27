1baf1cd25086d92c197a7884240fa7653c4533fb
emnt <abeme@example.com>
2024-05-27 21:19:59 +0300

    27.05.2024(urok)

---
+++ numbers_theory.py
@@ -0,0 +1,24 @@
+age = 14
+candies_per_yer = 2
+total_candies = age * candies_per_yer
+
+print(f'{total_candies=}')
+print(f"{2 * 5=}")
+
+B_n = 100_000_000_000_000
+print(B_n)
+
+f_number = 1000
+s_number = 7
+
+print(f"{f_number - s_number}")
+
+m_result = f_number * s_number
+d_result = f_number / s_number
+
+s_sum = 0.1 + 0.2
+n_sum = f_number + s_number + s_sum
+wtf_1 = f_number - s_number
+
+super_wtf = f_number + s_number / s_sum - B_n + 1000-7
+print(super_wtf)
